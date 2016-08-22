f14139584ebeee49e2ae713892735a23a7107ee2
azaze111o <azaze111o@users.noreply.github.com>
2016-08-22 10:39:26 -0600

    Update backward_and_forward.py

--- google_foobar/backward_and_forward.py
+++ google_foobar/backward_and_forward.py
@@ -1,3 +1,17 @@
+"""
+====================
+Backward and forward
+====================
+
+The sign outside reads: Name no one man.
+
+"Escape. We must escape." Staring at the locked door of his cage, Beta Rabbit, spy and brilliant mathematician, has a revelation.
+"Of course! Name no one man - it's a palindrome! Palindromes are the key to opening this lock!"
+
+To help Beta Rabbit crack the lock, write a function answer(n) which returns the smallest positive integer
+base b, at least 2, in which the integer n is a palindrome. The input n will satisfy "0 <= n <= 1000."
+
+"""
 def baseN(num, b, numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
 	return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
 	
